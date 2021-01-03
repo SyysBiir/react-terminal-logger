d166cd6f571eb41c5402b284bd4f089bf26ac28f
Maxim Boyanov <syysbiir@gmail.com>
2021-01-03 17:32:52 +0900

    Update console-logger.js

--- console-logger.js
+++ console-logger.js
@@ -1,18 +1,35 @@
 async function fetch_ (in_, val, stack, save_logs) {
 	return new Promise(resolve => {
 		var data;
-		if(typeof val == 'object') {
-			data = {
-				string: "0",
-				save_logs: save_logs ? "1" : "0",
-				data: val,
-				stack: stack
+		if(val.length === 1) {
+			if(typeof val[0] == 'object') {
+				data = {
+					string: "0",
+					save_logs: save_logs ? "1" : "0",
+					data: val[0],
+					stack: stack
+				}
+			} else {
+				data = {
+					string: '1',
+					save_logs: save_logs ? "1" : "0",
+					data: val[0].toString(),
+					stack: stack
+				}
 			}
 		} else {
+			let allVal = "";
+			val.map((l)=>{
+				if(typeof l == 'object') {
+					allVal = allVal + " " + JSON.stringify(l);
+				} else {
+					allVal = allVal + " " + l.toString();
+				}
+			})
 			data = {
 				string: '1',
 				save_logs: save_logs ? "1" : "0",
-				data: val.toString(),
+				data: allVal,
 				stack: stack
 			}
 		}
@@ -35,33 +52,53 @@ module.exports = {
 		if(process.env.NODE_ENV === 'development') {
 			console.disableYellowBox = true;
 			if(opt.indexOf("log") !== -1) {
-				window.console.log = (val) => {
-					fetch_("log", val, new Error().stack.split('\n')[1].split('@')[0], save_logs);
+				window.console.log = (...val) => {
+					let var_ = [];
+					val.map((l)=>{
+						var_.push(l)
+					})
+					fetch_("log", var_, new Error().stack.split('\n')[1].split('@')[0], save_logs);
 				}
 			}
 			if(opt.indexOf("error") !== -1) {
-				window.console.error = (val) => {
-					fetch_("error", val, new Error().stack.split('\n')[1].split('@')[0], save_logs);
+				window.console.error = (...val) => {
+					let var_ = [];
+					val.map((l)=>{
+						var_.push(l)
+					})
+					fetch_("error", var_, new Error().stack.split('\n')[1].split('@')[0], save_logs);
 				}
 			}
 			if(opt.indexOf("info") !== -1) {
-				window.console.info = (val) => {
-					fetch_("info", val, new Error().stack.split('\n')[1].split('@')[0], save_logs);
+				window.console.info = (...val) => {
+					let var_ = [];
+					val.map((l)=>{
+						var_.push(l)
+					})
+					fetch_("info", var_, new Error().stack.split('\n')[1].split('@')[0], save_logs);
 				}
 			}
 			if(opt.indexOf("warn") !== -1) {
-				window.console.warn = (val) => {
-					fetch_("warn", val, new Error().stack.split('\n')[1].split('@')[0], save_logs);
+				window.console.warn = (...val) => {
+					let var_ = [];
+					val.map((l)=>{
+						var_.push(l)
+					})
+					fetch_("warn", var_, new Error().stack.split('\n')[1].split('@')[0], save_logs);
 				}
 			}
 			if(opt.indexOf("logr") !== -1) {
-				window.logr = (val) => {
-					fetch_("logr", val, new Error().stack.split('\n')[1].split('@')[0], save_logs);
+				window.logr = (...val) => {
+					let var_ = [];
+					val.map((l)=>{
+						var_.push(l)
+					})
+					fetch_("logr", var_, new Error().stack.split('\n')[1].split('@')[0], save_logs);
 				}
 			}
 		} else {
-			window.logr = (val) => {
-				console.log(val)
+			window.logr = (...val) => {
+				console.log(...val)
 			}
 		}
 	}
